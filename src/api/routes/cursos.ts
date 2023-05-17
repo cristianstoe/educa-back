babd5784681fdc91317e78befa55c9092fa512f5
Cristian Stoenica <cristian3300@gmail.com>
2023-05-17 11:16:41 -0300

    started cursos

--- src/api/routes/cursos.ts
+++ src/api/routes/cursos.ts
@@ -3,3 +3,46 @@ import fastify from 'fastify'
 
 const prisma = new PrismaClient()
 const app = fastify({ logger: true })
+
+interface Curso {
+  Nome: string
+  Tipo: string
+}
+
+export function cursoRoutes(app, options, done) {
+  // get all cursos
+  app.get('/cursos', async (request, reply) => {
+    const cursos = await prisma.curso.findMany()
+    reply.send(cursos)
+  })
+
+  // get curso by id
+  app.get('/cursos/:ID', async (request, reply) => {
+    let { ID } = request.params
+    ID = parseInt(ID)
+    const cursos = await prisma.curso.findUnique({
+      where: {
+        ID,
+      },
+    })
+    if (!cursos) {
+      reply
+        .code(404)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send({ message: 'Curso not found' })
+    }
+    reply.code(200).send(cursos)
+  })
+
+  // create new curso
+  app.post('/cursos', async (request, reply) => {
+    const { Nome, Tipo } = request.body as Curso
+    const curso = await prisma.curso.create({
+      data: {
+        Nome,
+        Tipo,
+      },
+    })
+    reply.code(201).send(curso)
+  })
+}
